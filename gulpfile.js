1e4fd1c3fea83f634946d46dc14365f6ad0b5e9d
BruceAWest <bruceallanwest@gmail.com>
2017-11-13 17:39:58 -0500

    add app directory and image files, add gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp =  require('gulp'),
+watch = require('gulp-watch');
+
+gulp.task('default', function(){
+  console.log("Hooray! You created a gulp task!");
+});
+
+gulp.task('html', function() {
+  console.log("Imagine something useful being done to your HMTL here.");
+});
+
+gulp.task('watch', function() {
+  
+});

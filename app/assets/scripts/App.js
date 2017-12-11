ef5f1fc168487513d168c2710f1492da117432a2
BruceAWest <bruceallanwest@gmail.com>
2017-12-11 16:20:52 -0500

    test App.js

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,2 +1,19 @@
-console.log('Hello, my name is John Doe and my favorite color is blue.')
-console.log('Hello, my name is Jane Smith and my favorite color is green.')
+function Person(fullName, favColor) {
+  this.name = fullName;
+  this.favoriteColor = favColor
+  this.greet = function() {
+    console.log("Hello, my name is " + this.name + " and my favorite color is " + this.favoriteColor + ".");
+  }
+}
+
+var john = new Person("John Doe", "blue");
+john.greet();
+
+var jane = new Person("Jane Smith", "green");
+jane.greet();
+
+var butch = new Person("Butch Cassidy", "black");
+butch.greet();
+
+var tanto = new Person("Tanto", "burnt sienna");
+tanto.greet();

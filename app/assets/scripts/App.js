79597ab3a9979bf031ddb89bfbe2cf5d17c9076c
BruceAWest <bruceallanwest@gmail.com>
2017-12-11 17:15:26 -0500

    install Webpack

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,19 +1,8 @@
-function Person(fullName, favColor) {
-  this.name = fullName;
-  this.favoriteColor = favColor
-  this.greet = function() {
-    console.log("Hello, my name is " + this.name + " and my favorite color is " + this.favoriteColor + ".");
-  }
-}
+var $ = require('jquery');
+var Person = require('./modules/Person');
 
 var john = new Person("John Doe", "blue");
 john.greet();
 
 var jane = new Person("Jane Smith", "green");
 jane.greet();
-
-var butch = new Person("Butch Cassidy", "black");
-butch.greet();
-
-var tanto = new Person("Tanto", "burnt sienna");
-tanto.greet();
